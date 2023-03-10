2716a40929b21fec0d7918aff1f95d75e8ee273a
GentleAndy <andystemidwebguru@gmail.com>
2023-03-10 04:26:44 -0800

    Added constants to handle http error codes

---
+++ mycontacts-backend/constants.js
@@ -0,0 +1,9 @@
+// constants file to handle all http error codes
+
+exports.constants = {
+     VALIDATION_ERROR: 400,
+     UNAUTHORIZED: 401,
+     FORBIDDEN: 403,
+     NOT_FOUND: 404,
+     SERVER_ERROR: 500
+}
