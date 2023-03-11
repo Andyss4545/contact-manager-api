45a2c8d54eac72d5649a54ce154f0a265666e596
GentleAndy <andystemidwebguru@gmail.com>
2023-03-11 08:36:26 -0800

    Imported the user routes

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -7,10 +7,11 @@ const app = express(); // assign the express function to a variable
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // inbuilt middleware to accept data from the client to the server
+app.use(express.json()); // inbuilt middleware to accept data inform of json from the client to the server
 // whenever we want to use middleware we make use of app.use
 // app.use is a middleware
 app.use(errorHandler) // import the errorHandler
+app.use('/api/users', require("./routes/userRoutes"))
 app.use('/api/contacts', require("./routes/contactroutes")) // require and import the contactroutes
 
 app.listen(PORT, () => {
