56120b82c2a03ba90264551cbb8fcbdf18e137a4
GentleAndy <andystemidwebguru@gmail.com>
2023-03-11 08:18:18 -0800

    Created and configured userSchema model

---
+++ mycontacts-backend/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    username: {
+         type: String,
+         required: [true , "Please add the username"]
+    },
+
+    email: {
+        type: String,
+        required: [true, "Please add the user email address"],
+        unique: [true, "Email address already exist"]
+    },
+
+    password: {
+          type: String,
+          required: [true, "Please add the user password"]
+    },
+
+}, {
+    timestamps: true
+});
+
+
+module.exports = mongoose.model('user', userSchema)
