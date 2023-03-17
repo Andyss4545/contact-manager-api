10f6907cff44be6d8f42780802240c57fbefab7e
GentleAndy <andystemidwebguru@gmail.com>
2023-03-17 03:58:22 -0700

    Added a Schema types to find ObjectId of user

--- mycontacts-backend/models/contactModel.js
+++ mycontacts-backend/models/contactModel.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 
 // contact Schema are fields you want to collect from the client
 const contactSchema = mongoose.Schema({
+
+      user_id: {
+            // id created in mongodb where we will find ObjectId
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+      },
+
       name: {
           type: String,
           required: [true, "Please add the contact name"],
